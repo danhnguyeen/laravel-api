ad9ac4ab919b094a418f4420400b6ad45e9b3b0c
Danh Nguyen <danhcn09a@gmail.com>
2019-03-28 22:33:16 +0700

    - Added Buyer Scope

--- app/Buyer.php
+++ app/Buyer.php
@@ -4,9 +4,22 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Transaction;
+use App\Scopes\BuyerScope;
 
 class Buyer extends User
 {
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new BuyerScope);
+    }
+    
     public function transactions() {
         return $this->hasMany(Transaction::class);
     }
